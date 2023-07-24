cb11a4152f248ae2eab4146827705ed752803753
Mavullayya <muthamavullayya.1001@gmail.com>
2023-07-24 21:29:00 +0000

    modified comments

--- Mavullayya_HR.sql
+++ Mavullayya_HR.sql
@@ -1,20 +1,17 @@
 -- HR_DEPARTMENT TABLE
 
-DESC HR_DEPARTMENT;
-
+`
 SELECT * FROM HR_DEPARTMENT;
 
 SET TIMING ON;
 
 -- QUERY 1: Write a SQL Query to find the employee count for each agency?
-
 SELECT AGENCY, COUNT(*) NO_OF_EMPLOYEES FROM HR_DEPARTMENT
 GROUP BY AGENCY ORDER BY NO_OF_EMPLOYEES; 
 -- Elapsed: 00:00:00:03
 
 
 -- QUERY 2: Write a SQL Query to list the employees who are all CONTRACTUAL WORKER?
-
 CREATE INDEX hr_position_title ON HR_DEPARTMENT ("Position Title");
 
 SELECT "Employee Name","Position Title" FROM HR_DEPARTMENT
@@ -23,7 +20,6 @@ WHERE "Position Title" = 'CONTRACTUAL WORKER';
 
 
 -- QUERY 3: Write a SQL QUERY to list the Employee name work under Agency ABE LINCOLN PRES LIBRARY MUS2?
-
 SELECT "Employee Name" FROM HR_DEPARTMENT
 WHERE AGENCY = 'ABE LINCOLN PRES LIBRARY MUS2';
 -- Elapsed: 00:00:00:03
@@ -36,7 +32,6 @@ GROUP BY "Position Title" ORDER BY NO_OF_EMPLOYEES;
 
 
 -- QUERY 5: Write a SQL Query to count the number of employees whose period pay rate is less than 50000.
-
 SELECT count(*) NO_OF_EMPLOYEES FROM HR_DEPARTMENT
 WHERE "Period Pay Rate" < 50000;
 -- Elapsed: 00:00:00:03
