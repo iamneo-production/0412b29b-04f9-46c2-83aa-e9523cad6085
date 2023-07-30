837ab6663cd2e2935f21768d4ec386770cb3d755
nagasaipurvaz <68222105+nagasaipurvaz@users.noreply.github.com>
2023-07-30 18:12:49 +0530

    Update check.sql

--- check.sql
+++ check.sql
@@ -1,5 +1,5 @@
 
-/* described  data base*/
+-- described  data base 
 
 desc HR_DEPARTMENT ;
 
@@ -33,8 +33,8 @@ WHERE AGENCY = 'ABE LINCOLN PRES LIBRARY & MUS'; --Elapsed:  00:00:00:05
 
 -- WRITE A SQL QUERY TO FIND THE EMPLOYEES COUNT IN EACH POSITION : 
 
-SELECT "Position Title",COUNT(*) no_of_employees FROM HR_DEPARTMENT 
-GROUP BY "Position Title"; --Elapsed:  00:00:00:06
+SELECT "Position Title",COUNT(*) as no_of_employees FROM HR_DEPARTMENT 
+GROUP BY "Position Title" order by asc; --Elapsed:  00:00:00:06
 
 -- Write a SQL Query to count the number of employees whose period pay rate is less than 50000 
 
